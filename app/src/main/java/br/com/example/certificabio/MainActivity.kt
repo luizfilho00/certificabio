080ccee1e92d2eb0273d26cae4d4f1bc2fefe795
Luiz Filho <luiizfiilhoo@gmail.com>
2021-10-03 01:38:02 -0400

    Initialize

---
+++ app/src/main/java/br/com/example/certificabio/MainActivity.kt
@@ -0,0 +1,14 @@
+package br.com.example.certificabio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import java.lang.RuntimeException
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
